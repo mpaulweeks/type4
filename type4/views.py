8b17617c3c0882072b5cbe9bbe94cb62f0ef8cee
M. Paul Weeks <mpaulweeks@wsip-70-165-189-131.lv.lv.cox.net>
2014-07-08 01:20:12 -0700

    fix view to point to new dir

--- type4/views.py
+++ type4/views.py
@@ -7,5 +7,5 @@ def index(request):
     all_cards = Card.objects.order_by('name')
     filtered_cards = list(c for c in all_cards if c.is_in_stack())
     context = {'filtered_cards': filtered_cards}
-    return render(request, 'cards/index.html', context)   
+    return render(request, 'type4/index.html', context)   
     
